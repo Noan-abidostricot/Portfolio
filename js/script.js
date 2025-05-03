d02cbd9972f2eeccaff94d35519dd8938f36f96c
Lylais <hollyleduc@protonmail.com>
2025-05-03 20:33:26 +0200

    project finish, correct bug about burger menu

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+
+    // Hide the navbar if the cross (bx-x) is toggled off
+    if (!menuIcon.classList.contains('bx-x')) {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'block';
+    }
+};
